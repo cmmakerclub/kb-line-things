3b8727048f5c44257db8a2469cf66c59082efeb2
Gr4vity4 <prawared.bw@gmail.com>
2019-08-15 13:42:45 +0700

    initial

---
+++ block_line_thing.js
@@ -0,0 +1,204 @@
+module.exports = function(Blockly) {
+  "use strict";
+
+  Blockly.Blocks["line_things_block"] = {
+    init: function() {
+      this.appendStatementInput("LINE_THINGS_BLOCK")
+        .setCheck(null)
+        .appendField(
+          new Blockly.FieldImage(
+            "https://image.flaticon.com/icons/svg/124/124027.svg",
+            24,
+            24,
+            "*"
+          )
+        )
+        .appendField("LINE THINGS");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
+
+  Blockly.Blocks["device_name_block"] = {
+    init: function() {
+      this.appendDummyInput()
+        .appendField("DEVICE NAME")
+        .appendField(new Blockly.FieldTextInput(""), "DEVICE_NAME");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
+
+  Blockly.Blocks["user_service_uuid_block"] = {
+    init: function() {
+      this.appendDummyInput()
+        .appendField("USER SERVICE UUID")
+        .appendField(
+          new Blockly.FieldTextInput("5fe8b0a0-3444-4805-8842-52c7a9b6d8a1"),
+          "USER_SERVICE_UUID"
+        );
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
+
+  Blockly.Blocks["psdi_service_uuid_block"] = {
+    init: function() {
+      this.appendDummyInput()
+        .appendField("PSDI SERVICE UUID")
+        .appendField(
+          new Blockly.FieldTextInput("e625601e-9e55-4597-a598-76018a0d293d"),
+          "PSDI_SERVICE_UUID"
+        );
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
+
+  Blockly.Blocks["psdi_characteristic_uuid_block"] = {
+    init: function() {
+      this.appendDummyInput()
+        .appendField("PSDI CHARACTERISTIC UUID")
+        .appendField(
+          new Blockly.FieldTextInput("26e2b12b-85f0-4f3f-9fdd-91d114270e6e"),
+          "PSDI_CHARACTERISTIC_UUID"
+        );
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
+
+  Blockly.Blocks["characteristic_blocks"] = {
+    init: function() {
+      this.appendStatementInput("CHARACTERISTIC_BLOCKS")
+        .setCheck(null)
+        .appendField("CHARACTERISTIC BLOCKS");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
+
+  Blockly.Blocks["create_characteristic_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("CREATE CHARACTERISTIC");
+      this.appendDummyInput()
+        .appendField("NAME")
+        .appendField(new Blockly.FieldTextInput(""), "CHARACTERISTIC_NAME");
+      this.appendDummyInput()
+        .appendField("VALUE")
+        .appendField(new Blockly.FieldTextInput(""), "CHARACTERISTIC_VALUE");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
+
+  Blockly.Blocks["ble_characteristic_callbacks_block"] = {
+    init: function() {
+      this.appendStatementInput("BLE_CHARACTERISTIC_CALLBACKS")
+        .setCheck(null)
+        .appendField("BLE CHARACTERISTIC CALLBACKS");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(120);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
+
+  // Blockly.Blocks["ble_message_receive_block"] = {
+  //   init: function() {
+  //     this.appendStatementInput("BLE_MESSAGE_RECEIVE")
+  //       .setCheck(null)
+  //       .appendField("MESSAGE RECEIVE")
+  //       .appendField(
+  //         new Blockly.FieldVariable("BLE_MESSAGE_RECEIVE"),
+  //         "BLE_MESSAGE_RECEIVE"
+  //       );
+  //     this.setPreviousStatement(true, null);
+  //     this.setColour(120);
+  //     this.setTooltip("");
+  //     this.setHelpUrl("");
+  //   }
+  // };
+
+  Blockly.Blocks["ble_message_receive_block"] = {
+    init: function() {
+      this.appendStatementInput("BLE_MESSAGE_RECEIVE_BLOCK")
+        .setCheck(null)
+        .appendField("MESSAGE RECEIVE")
+        .appendField(
+          new Blockly.FieldVariable("BLE_MESSAGE_RECEIVE"),
+          "BLE_MESSAGE_RECEIVE"
+        );
+      this.setPreviousStatement(true, null);
+      this.setColour(120);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
+
+  Blockly.Blocks["ble_device_connection_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("BLE DEVICE CONNECTION");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
+
+  Blockly.Blocks["ble_device_disconnection_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("BLE DEVICE DISCONNECTION");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
+
+  Blockly.Blocks["setup_services_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("SETUP SERVICES");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
+
+  Blockly.Blocks["start_advertising_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("START ADVERTISING");
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
+};
---
+++ generator_line_thing.js
@@ -0,0 +1,275 @@
+module.exports = function(Blockly) {
+  "use strict";
+
+  Blockly.JavaScript["line_things_block"] = function(block) {
+    var statements_line_things_block = Blockly.JavaScript.statementToCode(
+      block,
+      "LINE_THINGS_BLOCK"
+    );
+    // TODO: Assemble JavaScript into code variable.
+
+    var code = `
+	#EXTINC
+		#include <BLEServer.h>
+		#include <BLEDevice.h>
+		#include <BLEUtils.h>
+		#include <BLE2902.h>
+
+		#define BUTTON 0
+		#define LED1 16
+
+		BLEServer* thingsServer;
+		BLESecurity *thingsSecurity;
+		BLEService* userService;
+		BLEService* psdiService;
+		BLECharacteristic* psdiCharacteristic;
+		BLECharacteristic* writeCharacteristic;
+		BLECharacteristic* notifyCharacteristic;
+
+		bool deviceConnected = false;
+		bool oldDeviceConnected = false;
+
+		volatile int btnAction = 0;
+
+		// User service characteristics
+		#define WRITE_CHARACTERISTIC_UUID "E9062E71-9E62-4BC6-B0D3-35CDCD9B027B"
+		#define NOTIFY_CHARACTERISTIC_UUID "62FBD229-6EDD-4D1A-B554-5C4E1BB29169"
+	#END
+	#FUNCTION
+		class serverCallbacks: public BLEServerCallbacks {
+		  void onConnect(BLEServer* pServer) {
+		   deviceConnected = true;
+		  };
+
+		  void onDisconnect(BLEServer* pServer) {
+		    deviceConnected = false;
+		  }
+		};
+	#END
+	
+	Serial.begin(115200);
+	pinMode(LED1, OUTPUT);
+    digitalWrite(LED1, 0);
+    pinMode(BUTTON, INPUT_PULLUP);
+    attachInterrupt(BUTTON, buttonAction, CHANGE);
+
+	BLEDevice::init("");
+	BLEDevice::setEncryptionLevel(ESP_BLE_SEC_ENCRYPT_NO_MITM);
+
+	// Security Settings
+	BLESecurity *thingsSecurity = new BLESecurity();
+	thingsSecurity->setAuthenticationMode(ESP_LE_AUTH_BOND);
+	thingsSecurity->setCapability(ESP_IO_CAP_NONE);
+	thingsSecurity->setInitEncryptionKey(ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK);
+
+	setupServices();
+	startAdvertising();
+	Serial.println("Ready to Connect");
+	${statements_line_things_block}
+    `;
+    return code;
+  };
+
+  Blockly.JavaScript["device_name_block"] = function(block) {
+    var text_device_name = block.getFieldValue("DEVICE_NAME");
+    // TODO: Assemble JavaScript into code variable.
+
+    var code = `
+	#EXTINC
+		// Device Name: Maximum 30 bytes
+		#define DEVICE_NAME "${text_device_name}"
+	#END
+    `;
+    return code;
+  };
+
+  Blockly.JavaScript["user_service_uuid_block"] = function(block) {
+    var text_user_service_uuid = block.getFieldValue("USER_SERVICE_UUID");
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+	#EXTINC
+		// User service UUID: Change this to your generated service UUID
+		#define USER_SERVICE_UUID "${text_user_service_uuid}"
+	#END
+    `;
+    return code;
+  };
+
+  Blockly.JavaScript["psdi_service_uuid_block"] = function(block) {
+    var text_psdi_service_uuid = block.getFieldValue("PSDI_SERVICE_UUID");
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+	#EXTINC
+		// PSDI Service UUID: Fixed value for Developer Trial
+		#define PSDI_SERVICE_UUID "${text_psdi_service_uuid}"
+	#END
+    `;
+    return code;
+  };
+
+  Blockly.JavaScript["psdi_characteristic_uuid_block"] = function(block) {
+    var text_psdi_characteristic_uuid = block.getFieldValue(
+      "PSDI_CHARACTERISTIC_UUID"
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+	#EXTINC
+		// PSDI Service UUID: Fixed value for Developer Trial
+		#define PSDI_CHARACTERISTIC_UUID "${text_psdi_characteristic_uuid}"
+	#END
+    `;
+    return code;
+  };
+
+  Blockly.JavaScript["characteristic_blocks"] = function(block) {
+    var statements_characteristic_blocks = Blockly.JavaScript.statementToCode(
+      block,
+      "CHARACTERISTIC_BLOCKS"
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = "...;\n";
+    return code;
+  };
+
+  Blockly.JavaScript["create_characteristic_block"] = function(block) {
+    var text_characteristic_name = block.getFieldValue("CHARACTERISTIC_NAME");
+    var text_characteristic_value = block.getFieldValue("CHARACTERISTIC_VALUE");
+    // TODO: Assemble JavaScript into code variable.
+    var code = "...;\n";
+    return code;
+  };
+
+  Blockly.JavaScript["ble_characteristic_callbacks_block"] = function(block) {
+    var statements_ble_characteristic_callbacks = Blockly.JavaScript.statementToCode(
+      block,
+      "BLE_CHARACTERISTIC_CALLBACKS"
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+	#FUNCTION
+		class writeCallback: public BLECharacteristicCallbacks {
+			${statements_ble_characteristic_callbacks}
+		};
+	#END
+    `;
+    return code;
+  };
+
+  Blockly.JavaScript["ble_message_receive_block"] = function(block) {
+    var variable_ble_message_receive = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("BLE_MESSAGE_RECEIVE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    var statements_ble_message_receive_block = Blockly.JavaScript.statementToCode(
+      block,
+      "BLE_MESSAGE_RECEIVE_BLOCK"
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+	  void onWrite(BLECharacteristic *bleWriteCharacteristic) {
+	    std::string value = bleWriteCharacteristic->getValue();
+	    char ${variable_ble_message_receive} = (char)value[0];
+	    ${statements_ble_message_receive_block}
+	  }
+    `;
+    return code;
+  };
+
+  Blockly.JavaScript["ble_device_connection_block"] = function(block) {
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+	  // Connection
+	  if (deviceConnected && !oldDeviceConnected) {
+	    oldDeviceConnected = deviceConnected;
+	  }
+
+	  uint8_t btnValue;
+
+	  while (btnAction > 0 && deviceConnected) {
+	    btnValue = !digitalRead(BUTTON);
+	    btnAction = 0;
+	    notifyCharacteristic->setValue(&btnValue, 1);
+	    notifyCharacteristic->notify();
+	    delay(20);
+	  }
+    `;
+    return code;
+  };
+
+  Blockly.JavaScript["ble_device_disconnection_block"] = function(block) {
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+	  // Disconnection
+	  if (!deviceConnected && oldDeviceConnected) {
+	    delay(500); // Wait for BLE Stack to be ready
+	    thingsServer->startAdvertising(); // Restart advertising
+	    oldDeviceConnected = deviceConnected;
+	  }
+    `;
+    return code;
+  };
+
+  Blockly.JavaScript["setup_services_block"] = function(block) {
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+	#FUNCTION
+		void setupServices(void) {
+		  // Create BLE Server
+		  thingsServer = BLEDevice::createServer();
+		  thingsServer->setCallbacks(new serverCallbacks());
+
+		  // Setup User Service
+		  userService = thingsServer->createService(USER_SERVICE_UUID);
+		  // Create Characteristics for User Service
+		  writeCharacteristic = userService->createCharacteristic(WRITE_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_WRITE);
+		  writeCharacteristic->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
+		  writeCharacteristic->setCallbacks(new writeCallback());
+
+		  notifyCharacteristic = userService->createCharacteristic(NOTIFY_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_NOTIFY);
+		  notifyCharacteristic->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
+		  BLE2902* ble9202 = new BLE2902();
+		  ble9202->setNotifications(true);
+		  ble9202->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
+		  notifyCharacteristic->addDescriptor(ble9202);
+
+		  // Setup PSDI Service
+		  psdiService = thingsServer->createService(PSDI_SERVICE_UUID);
+		  psdiCharacteristic = psdiService->createCharacteristic(PSDI_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ);
+		  psdiCharacteristic->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
+
+		  // Set PSDI (Product Specific Device ID) value
+		  uint64_t macAddress = ESP.getEfuseMac();
+		  psdiCharacteristic->setValue((uint8_t*) &macAddress, sizeof(macAddress));
+
+		  // Start BLE Services
+		  userService->start();
+		  psdiService->start();
+		}
+	#END
+    `;
+    return code;
+  };
+
+  Blockly.JavaScript["start_advertising_block"] = function(block) {
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+	#FUNCTION
+		void startAdvertising(void) {
+		  // Start Advertising
+		  BLEAdvertisementData scanResponseData = BLEAdvertisementData();
+		  scanResponseData.setFlags(0x06); // GENERAL_DISC_MODE 0x02 | BR_EDR_NOT_SUPPORTED 0x04
+		  scanResponseData.setName(DEVICE_NAME);
+
+		  thingsServer->getAdvertising()->addServiceUUID(userService->getUUID());
+		  thingsServer->getAdvertising()->setScanResponseData(scanResponseData);
+		  thingsServer->getAdvertising()->start();
+		}
+
+		void buttonAction() {
+		  btnAction++;
+		}
+	#END
+    `;
+    return code;
+  };
+};
